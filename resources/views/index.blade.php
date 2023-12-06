27a854611633f8185395b3a54a63e86cc4f50015
Deha <dehasuer97@gmail.com>
2023-12-06 19:12:20 +0300

    added some styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,21 +3,23 @@
 @section('title' , 'Tasks')
 
 @section('content')
-    <div>
-        <a href="{{ route('tasks.create') }}">Add Task</a>
-    </div>
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}"
+        class="font-medium text-gray-700 underline decoration-pink-500">Add Task</a>
+    </nav>
 <h2>The list of tasks</h2>
 <div>
         @forelse ($tasks as $task)
             <div>
-                <a href="{{ route('tasks.show' , ['task' => $task->id]) }}">{{$task->title}}</a>
+                <a href="{{ route('tasks.show' , ['task' => $task->id]) }}"
+                @class(['line-through' => $task->completed])>{{$task->title}}</a>
             </div>
         @empty
             <div>There are no tasks</div>
         @endforelse
 
     @if($task->count())
-        <nav>
+        <nav class="mt-4">
             {{$tasks->links()}}
         </nav>
     @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,11 @@
     <head>
         <title>Laravel 10 Task List App</title>
         @yield('styles')
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
 
-    <body>
-        <h1>@yield('title')</h1>
+    <body class="container mx-auto mt-10 mb-10 max-w-lg">
+        <h1 class="text-2xl mb-4">@yield('title')</h1>
         <div>
             @if(session()->has('success'))
                 <div>{{ session('success') }}</div>
