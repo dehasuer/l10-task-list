2989a8ee7c5cac24f5ffb78ef7e1e3035f9d362c
Deha <dehasuer97@gmail.com>
2023-12-06 17:27:27 +0300

    edited blade for more resuable

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -15,11 +15,14 @@
 
     <form method="POST" action="{{isset($task) ? route('tasks.update' , ['$task' => $task->id]) : route('tasks.store') }}">
         @csrf
+        @isset($task)
+            @method('PUT')
+        @endisset
         <div>
             <label for="title">
                 Title
             </label>
-            <input text="text" name="title" id="title" value="{{old('title')}}" />
+            <input text="text" name="title" id="title" value="{{ $task->title ?? old('title')}}" />
             @error('title')
             <p class="error-message">{{ $message }}</p>
             @enderror
@@ -27,7 +30,7 @@
 
         <div>
             <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5">{{old('description')}}</textarea>
+            <textarea name="description" id="description" rows="5">{{ $task->description ?? old('description')}}</textarea>
             @error('description')
             <p class="error-message">{{ $message }}</p>
             @enderror
@@ -35,14 +38,20 @@
 
         <div>
             <label for="long_description">Long Description</label>
-            <textarea name="long_description" id="long_description" rows="10">{{old('long_description')}}</textarea>
+            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('long_description')}}</textarea>
             @error('long_description')
             <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
 
         <div>
-            <button type="submit">Add Task</button>
+            <button type="submit">
+                @isset($task)
+                    Update Task
+                @else
+                    Add Task
+                @endisset
+            </button>
         </div>
     </form>
 
